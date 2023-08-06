e2aabd9819101afb2aa03c94ffdccf9a384c12a4
sofiauldemiaclear <sofy.torresv@gmail.com>
2023-08-06 03:44:18 -0500

    ADD: folders and settings with firebase

---
+++ src/Pages/wall/wall.jsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+import './wall.css';
+
+const Wall = () => {
+ 
+  return (
+    <div className="login">
+      <main>
+        <section className="login__body">
+          <h1 className='login__wall'>Este es el muro</h1>
+          
+        </section>
+      </main>
+    </div>
+  );
+};
+
+
+export default Wall;
